f3c81d986e4431e66c032d10faad51a972e5e95d
DongGyun <wooheuy5752@gmail.com>
2025-01-09 17:02:02 +0900

    feat: PostAdditionButton 스타일 변경 (#10)

--- src/components/domain/post/PostAdditionButton.tsx
+++ src/components/domain/post/PostAdditionButton.tsx
@@ -8,11 +8,11 @@ export const PostAdditionButton = ({ onClick }: PostAdditionButtonProps) => {
   return (
     <button
       type="button"
-      className="flex-center absolute bottom-[88px] left-1/2 -translate-x-1/2 gap-1 rounded-2xl border-2 border-grey-200 bg-grey-100 py-2 pl-[10px] pr-4 shadow-sm"
+      className="flex-center absolute bottom-[88px] left-1/2 -translate-x-1/2 gap-1 rounded-2xl border-2 border-black-100 py-2 pl-[10px] pr-4 shadow-sm"
       onClick={onClick}
     >
       <AdditionIcon />
-      <p className="p-small font-medium">글 올리기</p>
+      <p className="p-800 text-black-600">글 올리기</p>
     </button>
   )
 }
