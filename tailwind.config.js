fc7af72a458a15288289920b02148ab8bc9921ed
DongGyun <wooheuy5752@gmail.com>
2025-01-09 16:06:15 +0900

    chore: 새로운 색상 추가 (#9)

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ export default {
         jalnan: ['Jalnan2'],
       },
       colors: {
+        black: {
+          100: '#F3F3F3',
+          200: '#D2D0CD',
+          300: '#BAB7B3',
+          400: '#A09B96',
+          500: '#7F7973',
+          600: '#595551',
+          700: '#4B4641',
+        },
         blue: {
           100: '#D9F1FE',
           200: '#B3DFFE',
@@ -41,6 +50,9 @@ export default {
           200: '#FF5A28',
         },
         green: '#9CDE70',
+        success: '#78BE7B',
+        error: '#F3614E',
+        orange: '#FF8E5D',
       },
       utilities: {
         '.webkit-overflow-scrolling-touch': {
