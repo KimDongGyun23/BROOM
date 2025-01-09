db0626b5a1f84a0a2f2deb071c08cead1f081865
DongGyun <wooheuy5752@gmail.com>
2025-01-10 00:42:50 +0900

    feat: 상세페이지 가져오기에서 오류 발생 시, 에러페이지로 이동 (#11)

--- src/components/container/home/ErrorPage.tsx
+++ src/components/container/home/ErrorPage.tsx
@@ -4,13 +4,17 @@ import { Button } from '@/components/view/Button'
 
 export const ErrorPage = () => {
   const navigate = useNavigate()
+  const goToHome = () => {
+    navigate('/home', { replace: true })
+  }
+
   return (
     <div className="grid size-full place-items-center">
       <div className="flex-column-align gap-8">
-        <h1 className="font-jalnan text-[64px] font-bold text-blue-600">404</h1>
-        <p className="p-medium font-medium text-blue-500">{`네트워크 에러가 발생했어요...\n재시도 부탁드립니다.`}</p>
+        <h2 className="font-jalnan text-[64px] text-black-600">404</h2>
+        <p className="p-700 text-black-500">{`네트워크 에러가 발생했어요...\n재시도 부탁드립니다.`}</p>
         <div className="w-full pt-20">
-          <Button size="lg" className="w-full" onClick={() => navigate('/home')}>
+          <Button size="lg" className="hover-scale w-full" onClick={goToHome}>
             홈으로 이동하기
           </Button>
         </div>
