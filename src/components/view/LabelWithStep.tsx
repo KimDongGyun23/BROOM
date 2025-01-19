d2459b0410d9504645d32f29cc6417adbc27ce76
DongGyun <wooheuy5752@gmail.com>
2025-01-19 22:52:26 +0900

    style: LabelWithStep 스타일 적용 (#13)

--- src/components/view/LabelWithStep.tsx
+++ src/components/view/LabelWithStep.tsx
@@ -1,19 +1,35 @@
+import styled from 'styled-components'
+
 type LabelWithStepProps = {
   currentStep: number
   totalStep: number
   label: string
 }
 
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin: ${({ theme }) => `0 ${theme.gap.xl}`};
+`
+
+const Label = styled.h4`
+  color: ${({ theme }) => theme.colors.black[700]};
+`
+
+const StepIndicator = styled.p`
+  font-size: ${({ theme }) => theme.fontSize[900]};
+  line-height: ${({ theme }) => theme.lineHeight[900]};
+  color: ${({ theme }) => theme.colors.blue[500]};
+`
+
 export const LabelWithStep = ({ currentStep, totalStep, label }: LabelWithStepProps) => {
   return (
-    <div className="flex-between mx-4 mt-6 items-end">
-      <h4 className="font-bold text-grey-700">{label}</h4>
-      <p
-        className="p-small font-medium text-blue-400"
-        aria-label={`총 ${totalStep} 단계 중 ${currentStep} 단계`}
-      >
+    <Container>
+      <Label>{label}</Label>
+      <StepIndicator aria-label={`총 ${totalStep} 단계 중 ${currentStep} 단계`}>
         {currentStep} / {totalStep}
-      </p>
-    </div>
+      </StepIndicator>
+    </Container>
   )
 }
