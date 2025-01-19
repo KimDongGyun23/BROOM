a058448e4bb30a5213f9778c84199ea593ba1b77
DongGyun <wooheuy5752@gmail.com>
2025-01-19 22:55:15 +0900

    style: Loading 컴포넌트 스타일 적용 (#13)

--- src/components/view/Loading.tsx
+++ src/components/view/Loading.tsx
@@ -1,9 +1,20 @@
 import { SyncLoader } from 'react-spinners'
+import styled from 'styled-components'
+
+import theme from '@/styles/theme'
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
 
 export const Loading = () => {
   return (
-    <div className="flex-center size-full">
-      <SyncLoader color="#4196FD" />
-    </div>
+    <LoadingContainer>
+      <SyncLoader color={theme.colors.black[600]} />
+    </LoadingContainer>
   )
 }
