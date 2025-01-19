b6d0d4c50fb2e50f6535733994f8e16e11aa92e6
DongGyun <wooheuy5752@gmail.com>
2025-01-19 22:05:24 +0900

    feat: EmptyMessage 스타일 적용 (#13)

--- src/components/view/Error.tsx
+++ src/components/view/Error.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components'
+
 type EmptyMessageProps = {
   label: string
 }
 
+const StyledEmptyMessage = styled.p`
+  font-size: ${({ theme }) => theme.fontSize[700]};
+  line-height: ${({ theme }) => theme.lineHeight[700]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: ${({ theme }) => theme.gap.xxl};
+  color: ${({ theme }) => theme.colors.black[500]};
+`
+
 export const EmptyMessage = ({ label }: EmptyMessageProps) => {
-  return <p className="p-700 flex-center size-full p-5 text-black-500">{label}</p>
+  return <StyledEmptyMessage>{label}</StyledEmptyMessage>
 }
