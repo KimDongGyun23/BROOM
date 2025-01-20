a7e0871b457fc46ddedf6aaf87d7ad665bc93f02
DongGyun <wooheuy5752@gmail.com>
2025-01-20 12:54:10 +0900

    style: 회원가입 완료 페이지 스타일 적용 (#13)

--- src/components/container/auth/SignupCompletePage.tsx
+++ src/components/container/auth/SignupCompletePage.tsx
@@ -1,20 +1,44 @@
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { Button } from '@/components/view/Button'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+
+const Logo = styled.h1`
+  margin-top: 30svh;
+  text-align: center;
+  font-family: 'jalnan', sans-serif;
+  font-size: 60px;
+  line-height: 44px;
+  color: ${({ theme }) => theme.colors.black[600]};
+`
+
+const Message = styled.p`
+  margin: 30px ${({ theme }) => theme.gap.xl} 0;
+  font-size: ${({ theme }) => theme.fontSize[600]};
+  line-height: ${({ theme }) => theme.lineHeight[600]};
+  text-align: center;
+  color: ${({ theme }) => theme.colors.black[500]};
+`
+
+const StyledButton = styled(Button)`
+  margin: auto ${({ theme }) => theme.gap.xl} 40px;
+`
 
 export const SignupCompletePage = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex-column h-full">
-      <h1 className="mt-[30svh] text-center font-jalnan text-6xl leading-[44px] text-blue-600">
-        BROOM
-      </h1>
-      <p className="p-large mx-4 mt-[30px] text-center font-bold text-blue-500">
-        회원가입이 완료되었습니다.
-      </p>
-      <Button size="lg" className="mx-4 mb-10 mt-auto" onClick={() => navigate('/login')}>
+    <Container>
+      <Logo>BROOM</Logo>
+      <Message>회원가입이 완료되었습니다.</Message>
+      <StyledButton size="lg" onClick={() => navigate('/login')}>
         로그인 페이지로 이동
-      </Button>
-    </div>
+      </StyledButton>
+    </Container>
   )
 }
