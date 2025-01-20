86e58d7942cb3adf50c46cd6b60fa18204c1b1ff
DongGyun <wooheuy5752@gmail.com>
2025-01-20 21:15:20 +0900

    style: App 스타일 적용 (#13)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 import { Splash } from './components/container/home/Splash'
 import { RouterComponent } from './components/view/RouterComponent'
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const ContentWrapper = styled.div`
+  position: relative;
+  height: 100svh;
+  width: 100%;
+  min-width: 320px;
+  max-width: 450px;
+  border-left: 1px solid ${({ theme }) => theme.colors.black[100]};
+  border-right: 1px solid ${({ theme }) => theme.colors.black[100]};
+  overflow-y: scroll;
+`
 
 function App() {
   const [showSplash, setShowSplash] = useState<boolean>(true)
@@ -14,11 +32,9 @@ function App() {
     return () => clearTimeout(timer)
   }, [])
   return (
-    <div className="flex-center">
-      <div className="scroll relative h-svh w-full min-w-[320px] max-w-[450px] border-x border-x-black-100">
-        {showSplash ? <Splash /> : <RouterComponent />}
-      </div>
-    </div>
+    <AppContainer>
+      <ContentWrapper>{showSplash ? <Splash /> : <RouterComponent />}</ContentWrapper>
+    </AppContainer>
   )
 }
 
