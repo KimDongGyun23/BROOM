637c376b7ea0db5a909b71f66a3174954a9d2224
DongGyun <wooheuy5752@gmail.com>
2025-02-13 21:24:39 +0900

    fix: ReserveCreate 모달 provider 추가 (#24)

--- src/components/container/bus/ReserveCreate.tsx
+++ src/components/container/bus/ReserveCreate.tsx
@@ -8,7 +8,7 @@ import { InputGroup } from '@/components/view/inputGroup'
 import { ModalWithOneButton } from '@/components/view/Modal'
 import { SubHeaderWithoutIcon } from '@/components/view/SubHeader'
 import { busCreateAttribute, useBusCreateForm } from '@/forms/useBusCreateForm'
-import { useModalActions, useModalState } from '@/stores/modal'
+import { ModalStoreProvider, useModalActions, useModalState } from '@/stores/modal'
 import { Container } from '@/styles/commonStyles'
 
 const ReserveCreateModal = () => {
@@ -39,35 +39,37 @@ export const ReserveCreate = () => {
   const handleCancel = useCallback(() => navigate(-1), [navigate])
 
   return (
-    <Container>
-      <SubHeaderWithoutIcon type="null" onClickCancel={handleCancel} />
-      <Title>예약 정보 입력</Title>
+    <ModalStoreProvider>
+      <Container>
+        <SubHeaderWithoutIcon type="null" onClickCancel={handleCancel} />
+        <Title>예약 정보 입력</Title>
 
-      <FormProvider {...formMethod}>
-        <StyledForm>
-          <InputGroup section={NAME.section}>
-            <InputGroup.Label label={NAME.label} />
-            <InputGroup.Input {...NAME.input} />
-          </InputGroup>
+        <FormProvider {...formMethod}>
+          <StyledForm>
+            <InputGroup section={NAME.section}>
+              <InputGroup.Label label={NAME.label} />
+              <InputGroup.Input {...NAME.input} />
+            </InputGroup>
 
-          <InputGroup section={STUDENT_ID.section}>
-            <InputGroup.Label label={STUDENT_ID.label} />
-            <InputGroup.Input {...STUDENT_ID.input} />
-          </InputGroup>
+            <InputGroup section={STUDENT_ID.section}>
+              <InputGroup.Label label={STUDENT_ID.label} />
+              <InputGroup.Input {...STUDENT_ID.input} />
+            </InputGroup>
 
-          <InputGroup section={PHONE_NUMBER.section}>
-            <InputGroup.Label label={PHONE_NUMBER.label} />
-            <InputGroup.Input {...PHONE_NUMBER.input} />
-          </InputGroup>
-        </StyledForm>
-      </FormProvider>
+            <InputGroup section={PHONE_NUMBER.section}>
+              <InputGroup.Label label={PHONE_NUMBER.label} />
+              <InputGroup.Input {...PHONE_NUMBER.input} />
+            </InputGroup>
+          </StyledForm>
+        </FormProvider>
 
-      <StyledButton size="lg" className="mx-4 mb-10 mt-2" onClick={onSubmit}>
-        예약하기
-      </StyledButton>
+        <StyledButton size="lg" className="mx-4 mb-10 mt-2" onClick={onSubmit}>
+          예약하기
+        </StyledButton>
 
-      <ReserveCreateModal />
-    </Container>
+        <ReserveCreateModal />
+      </Container>
+    </ModalStoreProvider>
   )
 }
 
