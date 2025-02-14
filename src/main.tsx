4f3de7d15de1d5df4f20ebc481b15dabd2431fbf
DongGyun <wooheuy5752@gmail.com>
2025-02-15 00:36:38 +0900

    chore: Vercel Web Analytics, Speed Insights 설치 (#25)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from './styles/globalStyles.ts'
@@ -24,6 +26,8 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
+        <Analytics />
+        <SpeedInsights />
       </ThemeProvider>
     </QueryClientProvider>
   </BrowserRouter>,
