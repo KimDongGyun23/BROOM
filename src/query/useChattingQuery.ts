321cbd56f75852906d0aed84778145c872e395f3
DongGyun <wooheuy5752@gmail.com>
2025-02-19 23:26:21 +0900

    refactor: 채팅페이지 api 게시판 타입 걷어내기 (#29)

--- src/query/useChattingQuery.ts
+++ src/query/useChattingQuery.ts
@@ -8,39 +8,30 @@ import type {
   CarpoolChattingRoomResponse,
   CarpoolExitChattingRoomRequest,
   ChattingListProfileType,
-  TeamChattingRoomRequest,
 } from '@/types/chatting'
 
 import { instance } from '.'
 
-const POST_PAGES = ['carpool'] as const
-type PostPageType = (typeof POST_PAGES)[number]
-
-type ChattingRoomRequest<T extends PostPageType> = T extends 'carpool'
-  ? CarpoolChattingRoomRequest
-  : TeamChattingRoomRequest
+type ChattingRoomRequest = CarpoolChattingRoomRequest
 
 const API_ENDPOINTS = {
-  CHATTING_ID: (currentPage: PostPageType, id: string) => `/${currentPage}/chat/room/create/${id}`,
-  ROOM_INFO: (currentPage: PostPageType, id: string) => `/${currentPage}/chat/list/${id}`,
-  ROOM_LIST: (currentPage: PostPageType) => `/${currentPage}/chat/room/list`,
-  EXIT: (currentPage: PostPageType, id: string) => `/${currentPage}/chat/room/list/${id}`,
+  CHATTING_ID: (id: string) => `/chat/room/create/${id}`,
+  ROOM_INFO: (id: string) => `/chat/list/${id}`,
+  ROOM_LIST: `/chat/room/list`,
+  EXIT: (id: string) => `/chat/room/list/${id}`,
 } as const
 
 const queryKeys = {
   all: ['chatting'] as const,
-  roomList: (currentPage: PostPageType) =>
-    [...queryKeys.all, currentPage, 'chatting-list'] as const,
-  roomInfo: <T extends PostPageType>(currentPage: T, urls: ChattingRoomRequest<T>['urls']) =>
-    [...queryKeys.all, currentPage, ...Object.values(urls)] as const,
+  roomList: () => [...queryKeys.all, 'chatting-list'] as const,
+  roomInfo: (urls: ChattingRoomRequest['urls']) =>
+    [...queryKeys.all, ...Object.values(urls)] as const,
 }
 
 export const useCarpoolChattingRoomList = () => {
-  const currentPage = POST_PAGES[0]
-
   return useQuery<CarpoolChattingListResponse, Error, ChattingListProfileType[]>({
-    queryKey: queryKeys.roomList(currentPage),
-    queryFn: async () => await instance.get(API_ENDPOINTS.ROOM_LIST(currentPage)),
+    queryKey: queryKeys.roomList(),
+    queryFn: async () => await instance.get(API_ENDPOINTS.ROOM_LIST),
     gcTime: 0,
     staleTime: 0,
     select: (data) =>
@@ -57,30 +48,24 @@ export const useCarpoolChattingRoomList = () => {
 }
 
 export const useCarpoolChattingInfo = ({ urls }: CarpoolChattingRoomRequest) => {
-  const currentPage = POST_PAGES[0]
-
   return useQuery<CarpoolChattingRoomResponse, Error>({
-    queryKey: queryKeys.roomInfo(currentPage, urls),
-    queryFn: async () => await instance.get(API_ENDPOINTS.ROOM_INFO(currentPage, urls.chatRoomId)),
+    queryKey: queryKeys.roomInfo(urls),
+    queryFn: async () => await instance.get(API_ENDPOINTS.ROOM_INFO(urls.chatRoomId)),
   })
 }
 
 export const useCarpoolChattingId = () => {
-  const currentPage = POST_PAGES[0]
-
   return useMutation<CarpoolChattingIdResponse, Error, CarpoolChattingIdRequest>({
     mutationFn: async ({ urls }) =>
-      await instance.post(API_ENDPOINTS.CHATTING_ID(currentPage, urls.carpoolBoardId)),
+      await instance.post(API_ENDPOINTS.CHATTING_ID(urls.carpoolBoardId)),
   })
 }
 
 export const useCarpoolExitChattingRoom = () => {
-  const currentPage = POST_PAGES[0]
   const queryClient = useQueryClient()
 
   return useMutation<void, Error, CarpoolExitChattingRoomRequest>({
-    mutationFn: async ({ urls }) =>
-      await instance.post(API_ENDPOINTS.EXIT(currentPage, urls.chatRoomId)),
+    mutationFn: async ({ urls }) => await instance.post(API_ENDPOINTS.EXIT(urls.chatRoomId)),
     onSuccess: () => queryClient.invalidateQueries({ queryKey: queryKeys.all }),
   })
 }
