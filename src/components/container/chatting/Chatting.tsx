f025b7250ba7e7d88b204c07507d665310f96e1e
DongGyun <wooheuy5752@gmail.com>
2025-02-20 14:11:08 +0900

    feat: 채팅 목록 구현 (#29)

--- src/components/container/chatting/Chatting.tsx
+++ src/components/container/chatting/Chatting.tsx
@@ -1,5 +1,3 @@
-import styled from 'styled-components'
-
 import { CarpoolChattingList } from '@/components/domain/chatting/CarpoolChattingList'
 import { BottomNavigation } from '@/components/view/BottomNavigation'
 import { MainHeader } from '@/components/view/MainHeader'
@@ -9,17 +7,8 @@ export const Chatting = () => {
   return (
     <Container>
       <MainHeader />
-      <MainContent>
-        <CarpoolChattingList />
-      </MainContent>
+      <CarpoolChattingList />
       <BottomNavigation />
     </Container>
   )
 }
-
-const MainContent = styled.main`
-  ${({ theme }) => theme.flexBox('column', undefined, undefined, 'lg')};
-  ${({ theme }) => theme.margin('container', 0)};
-  flex-grow: 1;
-  overflow-y: scroll;
-`
--- src/components/domain/chatting/CarpoolChattingList.tsx
+++ src/components/domain/chatting/CarpoolChattingList.tsx
@@ -1,9 +1,37 @@
 import { Link } from 'react-router-dom'
+import { styled } from 'styled-components'
 
-import { Loading } from '@/components/view/Loading'
-import { useCarpoolChattingRoomList } from '@/query/useChattingQuery'
+import type { MilitaryBranchCode } from '@/utils/constants'
 
-import { ChattingItem } from './ChattingItem'
+import { ChattingProfileImageBox } from './ChattingProfileImageBox'
+
+type ChattingProfileProps = {
+  boardName: string
+  lastMessage: string
+  lastMessageTime: string
+  militaryBranches: MilitaryBranchCode[]
+  expelled: boolean
+}
+
+const ChattingItem = ({
+  boardName,
+  lastMessage,
+  lastMessageTime,
+  militaryBranches,
+  expelled,
+}: ChattingProfileProps) => (
+  <ChattingItemContainer>
+    <ChattingProfileImageBox profileIconList={militaryBranches} />
+    <ChattingInformation>
+      <ChattingPostTitle>{boardName}</ChattingPostTitle>
+      <LastMessage $expelled={expelled}>
+        {expelled ? '방장에 의해 내보내졌습니다.' : lastMessage}
+      </LastMessage>
+    </ChattingInformation>
+
+    {!expelled && <LastMessageTime>{lastMessageTime}</LastMessageTime>}
+  </ChattingItemContainer>
+)
 
 export const CarpoolChattingList = () => {
   const { data: chattingList, isPending, isError } = useCarpoolChattingRoomList()
@@ -12,20 +40,58 @@ export const CarpoolChattingList = () => {
   if (isError) return <div>error</div>
 
   return (
-    <>
-      {chattingList.map(
-        ({ id, opponent, militaryChaplain, title, lastMessage, lastMessageDaysAgo }) => (
-          <Link to={`/chatting/chatting-room/carpool/${id}`} key={id}>
-            <ChattingItem
-              opponent={opponent}
-              iconType={militaryChaplain}
-              title={title}
-              lastMessage={lastMessage}
-              lastMessageDaysAgo={lastMessageDaysAgo}
-            />
-          </Link>
-        ),
-      )}
-    </>
+    <ChattingListContainer>
+      {chattingList.map((chattingRoomInformation) => (
+        <Link
+          key={chattingRoomInformation.boardId}
+          to={`/chatting/chatting-room/${chattingRoomInformation.boardId}`}
+        >
+          <ChattingItem {...chattingRoomInformation} />
+        </Link>
+      ))}
+    </ChattingListContainer>
   )
 }
+
+const ChattingListContainer = styled.main`
+  ${({ theme }) => theme.flexBox('column')};
+  ${({ theme }) => theme.margin('container', 0)};
+  flex-grow: 1;
+  overflow-y: scroll;
+`
+
+const ChattingItemContainer = styled.div`
+  ${({ theme }) => theme.flexBox('row', 'center', undefined, 'lg')};
+  ${({ theme }) => theme.padding('lg', 'md')};
+  ${({ theme }) => theme.border('divider', 'bottom')};
+  width: 100%;
+`
+
+const ChattingInformation = styled.div`
+  ${({ theme }) => theme.flexBox('column', undefined, undefined, 'sm')};
+  min-width: 0;
+  flex-grow: 1;
+`
+
+const ChattingPostTitle = styled.div`
+  ${({ theme }) => theme.font(600, theme.colors.black[600])};
+  min-width: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const LastMessage = styled.p<{ $expelled?: boolean }>`
+  ${({ theme, $expelled }) =>
+    theme.font(800, $expelled ? theme.colors.orange : theme.colors.black[300])};
+  min-width: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const LastMessageTime = styled.p`
+  ${({ theme }) => theme.margin(0, 0, 0, 'auto')};
+  ${({ theme }) => theme.font(800, theme.colors.black[300])};
+  flex-shrink: 0;
+`
