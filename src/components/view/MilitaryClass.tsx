590bbfe459d182d24d1b8cb109e41e0553d8270b
DongGyun <wooheuy5752@gmail.com>
2025-02-20 13:19:06 +0900

    feat: MilitaryClass 생성 (#29)
    연차별 계급을 나타내는 아이콘

---
+++ src/components/view/MilitaryClass.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+import { SkullIcon } from './icons/NonActiveIcons'
+
+type MilitaryClassProps = {
+  reserveYear: number
+}
+
+export const MilitaryClass = ({ reserveYear }: MilitaryClassProps) => {
+  return (
+    <Container>
+      {reserveYear === 0 ? (
+        <SkullIcon />
+      ) : (
+        Array.from({ length: reserveYear }, (_, index) => <Line key={index} />)
+      )}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  ${({ theme }) => theme.flexBox('column', undefined, 'space-around')};
+  width: 20px;
+  height: 20px;
+`
+
+const Line = styled.div`
+  ${({ theme }) => theme.borderRadius('xs')};
+  height: 3px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+`
