6bc96445bb4bebbba6b7ef4a6abd369daf7a6097
DongGyun <wooheuy5752@gmail.com>
2025-02-20 16:14:03 +0900

    refactor: 메세지 스토어 수정 (#29)
    MessageStore에서 ChatMessageStore로 변경 후, 내부 함수명 수정

--- src/stores/chatMessage.ts
+++ src/stores/chatMessage.ts
@@ -3,24 +3,24 @@ import { create } from 'zustand'
 import type { MessageType } from '@/types/chatting'
 
 type Actions = {
-  initialMessage: (messages: MessageType[]) => void
+  setInitialMessage: (messages: MessageType[]) => void
   addMessage: (message: MessageType) => void
   sendMessage: (message: MessageType) => void
 }
 
-type MessageStore = {
+type ChatMessageStore = {
   messages: MessageType[]
   actions: Actions
 }
 
-const useMessageStore = create<MessageStore>((set) => ({
+const useChatMessageStore = create<ChatMessageStore>((set) => ({
   messages: [],
   actions: {
-    initialMessage: (messages) => set(() => ({ messages: [...messages] })),
+    setInitialMessage: (messages) => set(() => ({ messages: [...messages] })),
     addMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
     sendMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
   },
 }))
 
-export const useMessageData = () => useMessageStore((state) => state.messages)
-export const useMessageActions = () => useMessageStore((state) => state.actions)
+export const useChatMessages = () => useChatMessageStore((state) => state.messages)
+export const useChatMessageActions = () => useChatMessageStore((state) => state.actions)
