fd248dc397b78b16e298a39ec18091d63e7ab75c
DongGyun <wooheuy5752@gmail.com>
2025-02-22 21:29:18 +0900

    feat: 내보내기 시, 사이드바 쿼리 재요청 (#29)

--- src/components/domain/chatting/ChatExpelButton.tsx
+++ src/components/domain/chatting/ChatExpelButton.tsx
@@ -3,7 +3,12 @@ import { styled } from 'styled-components'
 import { ModalWithOneButton } from '@/components/view/modal/ButtonModal'
 import { useParamId } from '@/hooks/useParamId'
 import { useExpelUser, useFetchChatSidebarInformation } from '@/query/useChattingQuery'
-import { ModalStoreProvider, useModalActions, useModalState } from '@/stores/modal'
+import {
+  ModalStoreProvider,
+  useIsSuccessModal,
+  useModalActions,
+  useModalState,
+} from '@/stores/modal'
 
 type ChatExpelButtonProps = {
   userId: string
@@ -12,21 +17,16 @@ type ChatExpelButtonProps = {
 const ExpelButton = ({ userId }: ChatExpelButtonProps) => {
   const boardId = useParamId()
 
-  const { mutate: expelUser } = useExpelUser()
-  const { refetch } = useFetchChatSidebarInformation({ urls: { boardId } })
-
   const { openModal } = useModalActions()
+
+  const { mutate: expelUser } = useExpelUser()
 
   const handleClickExpelButton = () => {
     expelUser(
       { body: { expellId: userId, boardId } },
       {
-        onSuccess: (response) => {
-          // refetch 에러처리 필요
-          refetch()
-          openModal(response)
-        },
-        onError: (error) => openModal(error.message),
+        onSuccess: (response) => openModal(response, true),
+        onError: (error) => openModal(error.message, false),
       },
     )
   }
@@ -35,15 +35,26 @@ const ExpelButton = ({ userId }: ChatExpelButtonProps) => {
 }
 
 const ExpelModal = () => {
+  const boardId = useParamId()
+
+  const isSuccessModal = useIsSuccessModal()
   const { isModalOpen, label } = useModalState()
   const { closeModal } = useModalActions()
+
+  const { refetch } = useFetchChatSidebarInformation({ urls: { boardId } })
+
+  const handleCloseModal = () => {
+    closeModal()
+    // refetch 에러처리 필요
+    refetch()
+  }
 
   return (
     <ModalWithOneButton
       isOpen={isModalOpen}
       content={label}
       onClose={closeModal}
-      button={{ onClick: closeModal, label: '확인' }}
+      button={{ onClick: isSuccessModal ? handleCloseModal : closeModal, label: '확인' }}
     />
   )
 }
