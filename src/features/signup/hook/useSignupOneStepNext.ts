a779c5c23359fa6034698aa134d152d4753dd1f4
DongGyun <wooheuy5752@gmail.com>
2025-03-05 21:49:19 +0900

    test: 회원가입 다음 단계 버튼 테스트 (#37)

--- src/features/signup/hook/useSignupOneStepNext.ts
+++ src/features/signup/hook/useSignupOneStepNext.ts
@@ -1,4 +1,3 @@
-import { useCallback } from 'react'
 import { useFormContext } from 'react-hook-form'
 
 import { signupAttribute } from '@/entities/auth/config/auth.schema'
@@ -6,15 +5,23 @@ import { useIdUniqueState } from '@/features/check-id-duplication/model/idDuplic
 import { useStepsActions } from '@/shared/model/steps.store'
 
 export const useSignupOneStepNext = () => {
-  const { trigger } = useFormContext()
+  const {
+    trigger,
+    formState: { errors },
+  } = useFormContext()
+
   const { goNextStep } = useStepsActions()
+
   const isIdUnique = useIdUniqueState()
+
   const { ID, PASSWORD, CONFIRM } = signupAttribute
 
-  const handleNextStep = useCallback(async () => {
+  console.log(errors)
+
+  const handleNextStep = async () => {
     const isValid = await trigger([ID.section, PASSWORD.section, CONFIRM.section])
     if (isValid && isIdUnique) goNextStep()
-  }, [trigger, ID.section, PASSWORD.section, CONFIRM.section, isIdUnique, goNextStep])
+  }
 
   return { handleNextStep }
 }
--- src/features/signup/hook/useSignupTwoStepNext.ts
+++ src/features/signup/hook/useSignupTwoStepNext.ts
@@ -1,4 +1,3 @@
-import { useCallback } from 'react'
 import { useFormContext } from 'react-hook-form'
 
 import { signupAttribute } from '@/entities/auth/config/auth.schema'
@@ -7,25 +6,21 @@ import { useStepsActions } from '@/shared/model/steps.store'
 
 export const useSignupTwoStepNext = () => {
   const { trigger } = useFormContext()
+
   const { goNextStep } = useStepsActions()
+
   const isNicknameUnique = useNicknameUniqueState()
+
   const { NICKNAME, DISCHARGE_YEAR, MILITARY_BRANCH } = signupAttribute
 
-  const handleNextStep = useCallback(async () => {
+  const handleNextStep = async () => {
     const isValid = await trigger([
       NICKNAME.section,
       DISCHARGE_YEAR.section,
       MILITARY_BRANCH.section,
     ])
     if (isValid && isNicknameUnique) goNextStep()
-  }, [
-    trigger,
-    NICKNAME.section,
-    DISCHARGE_YEAR.section,
-    MILITARY_BRANCH.section,
-    isNicknameUnique,
-    goNextStep,
-  ])
+  }
 
   return { handleNextStep }
 }
