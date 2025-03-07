e5a99b72ed0d5760610b0daead61eeab6688a519
DongGyun <wooheuy5752@gmail.com>
2025-03-07 09:42:43 +0900

    fix: 로그인 모달 생성 (#38)

--- src/features/login/ui/LoginButton.tsx
+++ src/features/login/ui/LoginButton.tsx
@@ -1,5 +1,6 @@
 import { ModalStoreProvider } from '@/shared/model/modal.store'
 import { Button } from '@/shared/ui/Button'
+import { ModalWithOneButton } from '@/shared/ui/modal/ButtonModal'
 
 import { useLogin } from '../hook/useLogin'
 
@@ -7,9 +8,12 @@ const LoginButtonWithModal = () => {
   const { onSubmit } = useLogin()
 
   return (
-    <Button size="lg" onClick={onSubmit}>
-      로그인
-    </Button>
+    <>
+      <Button size="lg" onClick={onSubmit}>
+        로그인
+      </Button>
+      <ModalWithOneButton />
+    </>
   )
 }
 
