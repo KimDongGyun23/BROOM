1b2b32c38157404a84265552e85969640b7008ec
DongGyun <wooheuy5752@gmail.com>
2025-03-09 21:44:33 +0900

    refactor: 로그인 모달 수정 (#40)

--- src/features/login/hook/useLogin.ts
+++ src/features/login/hook/useLogin.ts
@@ -4,18 +4,17 @@ import axios from 'axios'
 
 import { instance } from '@/app/api'
 import type { LoginCredentials } from '@/entities/auth/model/auth.type'
-import { useModalActions } from '@/shared/model/modal.store'
+import type { OpenModal } from '@/shared/hook/useModal'
+import { MODAL_KEYS } from '@/shared/lib/constants'
 
 import { useLoginMutation } from '../api/useLogin.mutation'
 import { useAuthActions } from '../model/auth.store'
 
-export const useLogin = () => {
+export const useLogin = (openModal: OpenModal) => {
   const navigate = useNavigate()
 
   const { mutate: loginMutation } = useLoginMutation()
   const { login, logout } = useAuthActions()
-
-  const { openOneButtonModal } = useModalActions()
 
   const { handleSubmit } = useFormContext<LoginCredentials>()
 
@@ -35,9 +34,12 @@ export const useLogin = () => {
         },
         onError: (error: Error) => {
           if (axios.isAxiosError(error)) {
-            openOneButtonModal(error.response?.data || '알 수 없는 서버 오류가 발생했습니다.')
+            openModal(
+              MODAL_KEYS.error,
+              error.response?.data || '알 수 없는 서버 오류가 발생했습니다.',
+            )
           } else {
-            openOneButtonModal('알 수 없는 오류가 발생했습니다.')
+            openModal(MODAL_KEYS.error, '알 수 없는 오류가 발생했습니다.')
           }
         },
       },
--- src/features/login/ui/LoginButton.tsx
+++ src/features/login/ui/LoginButton.tsx
@@ -1,26 +1,26 @@
-import { ModalStoreProvider } from '@/shared/model/modal.store'
+import useModal from '@/shared/hook/useModal'
+import { MODAL_KEYS } from '@/shared/lib/constants'
 import { Button } from '@/shared/ui/Button'
 import { ModalWithOneButton } from '@/shared/ui/modal/ButtonModal'
 
 import { useLogin } from '../hook/useLogin'
 
-const LoginButtonWithModal = () => {
-  const { onSubmit } = useLogin()
+export const LoginButton = () => {
+  const { modalLabel, isModalOpen, openModal, closeModal } = useModal()
+  const { onSubmit } = useLogin(openModal)
 
   return (
     <>
       <Button size="lg" onClick={onSubmit}>
         로그인
       </Button>
-      <ModalWithOneButton />
-    </>
-  )
-}
 
-export const LoginButton = () => {
-  return (
-    <ModalStoreProvider>
-      <LoginButtonWithModal />
-    </ModalStoreProvider>
+      <ModalWithOneButton
+        label={modalLabel(MODAL_KEYS.error)}
+        isModalOpen={isModalOpen(MODAL_KEYS.error)}
+        closeModal={closeModal}
+        button={{ onClickButton: closeModal }}
+      />
+    </>
   )
 }
