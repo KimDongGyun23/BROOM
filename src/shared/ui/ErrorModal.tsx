276810d761dea40d7f7820c9014278f12e7198b6
DongGyun <wooheuy5752@gmail.com>
2025-03-10 01:28:52 +0900

    test: 전역 에러 처리 테스트 (#40)
    에러 모달 적용

--- src/shared/ui/ErrorModal.tsx
+++ src/shared/ui/ErrorModal.tsx
@@ -6,17 +6,17 @@ import { MODAL_KEYS } from '../lib/constants'
 
 import { ModalWithOneButton } from './modal/ButtonModal'
 
-export const ErrorModal = ({ error }: FallbackProps) => {
+export const ErrorModal = ({ error, resetErrorBoundary }: FallbackProps) => {
   const { response } = error
 
   const { modalLabel, isModalOpen, openModal, closeModal } = useModal()
 
   // const isNotAuthorized = status === 401 || status === 403
 
-  useEffect(
-    () => openModal(MODAL_KEYS.error, response?.data || '알 수 없는 오류입니다.'),
-    [openModal, response?.data],
-  )
+  useEffect(() => {
+    openModal(MODAL_KEYS.error, response?.data || '알 수 없는 오류입니다.')
+    resetErrorBoundary()
+  }, [openModal, resetErrorBoundary, response?.data])
 
   return (
     <ModalWithOneButton
