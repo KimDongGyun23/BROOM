591d314727fc36a6fb2b1dc0e75180333e09769c
DongGyun <wooheuy5752@gmail.com>
2025-03-10 09:57:31 +0900

    test: 전역 에러 처리 테스트 (#40)
    mutation 에러 적용

--- src/app/provider/CustomQueryClientProvider.tsx
+++ src/app/provider/CustomQueryClientProvider.tsx
@@ -2,7 +2,13 @@ import type { PropsWithChildren } from 'react'
 import { useMemo } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import useModal from '@/shared/hook/useModal'
+import { MODAL_KEYS } from '@/shared/lib/constants'
+import { ModalWithOneButton } from '@/shared/ui/modal/ButtonModal'
+
 export const CustomQueryClientProvider = ({ children }: PropsWithChildren) => {
+  const { modalLabel, isModalOpen, openModal, closeModal } = useModal()
+
   const queryClient = useMemo(
     () =>
       new QueryClient({
@@ -15,12 +21,25 @@ export const CustomQueryClientProvider = ({ children }: PropsWithChildren) => {
             retry: 0,
           },
           mutations: {
-            throwOnError: true,
+            // throwOnError: true,
+            onError(error) {
+              openModal(MODAL_KEYS.error, error.message)
+            },
           },
         },
       }),
-    [],
+    [openModal],
   )
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ModalWithOneButton
+        label={modalLabel(MODAL_KEYS.error)}
+        isModalOpen={isModalOpen(MODAL_KEYS.error)}
+        closeModal={closeModal}
+        button={{ onClickButton: closeModal }}
+      />
+    </QueryClientProvider>
+  )
 }
