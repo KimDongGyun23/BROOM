46cafdb4bbcc041798ea423dbfb938e676c8ffba
DongGyun <wooheuy5752@gmail.com>
2025-04-04 10:53:37 +0900

    feat: instanceWithoutAuth 생성 및 로그인 적용 (#52)

--- src/features/login/api/useLogin.mutation.ts
+++ src/features/login/api/useLogin.mutation.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 
-import { instanceWithoutAuth } from '@/app/api'
+import { instanceWithoutAuth } from '@/app/api/instanceWithoutAuth'
 import type { LoginRequest, LoginResponse } from '@/entities/auth/model/auth.type'
 
 export const useLoginMutation = () =>
--- src/features/login/hook/useLogin.ts
+++ src/features/login/hook/useLogin.ts
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom'
 
-import { instance } from '@/app/api'
 import type { LoginCredentials } from '@/entities/auth/model/auth.type'
 
 import { useLoginMutation } from '../api/useLogin.mutation'
@@ -17,10 +16,9 @@ export const useLogin = () => {
       { body: { ...formData } },
       {
         onSuccess: (response) => {
-          instance.setAccessToken(response.headers['authorization'])
-          login(response.data)
-          if (response.data.role === 'ROLE_MEMBER') navigate('/home', { replace: true })
-          else if (response.data.role === 'ROLE_ADMIN') navigate('/kw/broom', { replace: true })
+          login(response)
+          if (response.role === 'ROLE_MEMBER') navigate('/home', { replace: true })
+          else if (response.role === 'ROLE_ADMIN') navigate('/kw/broom', { replace: true })
           else {
             logout()
             navigate('/404', { replace: true })
