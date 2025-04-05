15dfd31f5528e76507adc8c03adac8d190a7a25d
DongGyun <wooheuy5752@gmail.com>
2025-04-05 17:58:28 +0900

    test: 로그아웃 후 뒤로가기 시, 이벤트 발생 (#54)

--- src/features/logout/hook/useLogout.ts
+++ src/features/logout/hook/useLogout.ts
@@ -2,26 +2,35 @@ import { useNavigate } from 'react-router-dom'
 
 import { instance } from '@/app/api'
 import { useAuthActions } from '@/features/login/model/auth.store'
+import type { OpenModal } from '@/shared/hook/useModal'
+import { MODAL_KEYS } from '@/shared/lib/constants'
 
 import { useLogoutMutation } from '../api/useLogout.mutation'
 
-export const useLogout = (closeModal: VoidFunction) => {
+export const useLogout = (openModal: OpenModal, closeModal: VoidFunction) => {
   const navigate = useNavigate()
 
   const { mutate: logoutMutation } = useLogoutMutation()
 
   const { logout } = useAuthActions()
 
-  const handleClickLogout = () => {
+  const handleOpenModal = () => {
     logoutMutation(undefined, {
       onSuccess: () => {
-        navigate('/home')
+        openModal(MODAL_KEYS.success, '로그아웃에 성공했습니다.')
         instance.resetAccessToken()
-        closeModal()
-        logout()
+        window.history.pushState({ isLogoutModalOpen: true }, '')
       },
     })
   }
 
-  return { handleClickLogout }
+  const handleClickModal = () => {
+    closeModal()
+    navigate('/home')
+    logout()
+
+    if (window.history.state?.isLogoutModalOpen) window.history.replaceState({}, '')
+  }
+
+  return { handleOpenModal, handleClickModal }
 }
--- src/features/logout/ui/LogoutButton.tsx
+++ src/features/logout/ui/LogoutButton.tsx
@@ -1,27 +1,38 @@
+import { useEffect } from 'react'
 import { styled } from 'styled-components'
 
 import { useLogout } from '@/features/logout/hook/useLogout'
 import useModal from '@/shared/hook/useModal'
 import { MODAL_KEYS } from '@/shared/lib/constants'
-import { ModalWithTwoButton } from '@/shared/ui/modal/ButtonModal'
+import { ModalWithOneButton } from '@/shared/ui/modal/ButtonModal'
 
 export const LogoutButton = () => {
   const { modalLabel, isModalOpen, openModal, closeModal } = useModal()
 
-  const { handleClickLogout } = useLogout(closeModal)
+  const { handleOpenModal, handleClickModal } = useLogout(openModal, closeModal)
 
-  const handleOpenModal = () => openModal(MODAL_KEYS.success, '로그아웃하시겠습니까?')
+  useEffect(() => {
+    if (isModalOpen(MODAL_KEYS.success)) {
+      const handlePopState = (event: PopStateEvent) => {
+        if (event.state?.isLogoutModalOpen) {
+          handleClickModal()
+        }
+      }
+
+      window.addEventListener('popstate', handlePopState)
+      return () => window.removeEventListener('popstate', handlePopState)
+    }
+  }, [handleClickModal, isModalOpen])
 
   return (
     <>
       <StyledButton onClick={handleOpenModal}>로그아웃</StyledButton>
 
-      <ModalWithTwoButton
+      <ModalWithOneButton
         label={modalLabel(MODAL_KEYS.success)}
         isModalOpen={isModalOpen(MODAL_KEYS.success)}
-        closeModal={closeModal}
-        primaryButton={{ buttonLabel: '확인', onClickButton: handleClickLogout }}
-        secondaryButton={{ buttonLabel: '취소', onClickButton: closeModal }}
+        closeModal={handleClickModal}
+        button={{ onClickButton: handleClickModal }}
       />
     </>
   )
